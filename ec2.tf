bb0bae95a0b6462f9616e6d5b58b8799a4b359b5
harsha4672 <l.harsha7658@gmail.com>
2025-04-25 07:50:52 -0400

    modified ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "first-tf-instance" {
   
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "deployer-key"
   monitoring             = true
   ami = "ami-084568db4383264d4"
 
@@ -24,4 +24,9 @@ resource "aws_instance" "first-tf-instance" {
     Terraform   = "true"
     Environment = "dev"
   }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEeEFCZrFcB7JdtOwtvBpqayPXZ+Bcwc6D0R30y1NP1w l.harsha7658@gmail.com"
 }
